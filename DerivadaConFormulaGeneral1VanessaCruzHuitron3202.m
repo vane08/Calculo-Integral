50b49f4173ebd728336f160da051a654a9355d0a
vane08 <82453121+vane08@users.noreply.github.com>
2021-04-17 12:47:03 -0500

    Add files via upload

---
+++ DerivadaConFormulaGeneral1VanessaCruzHuitron3202.m
@@ -0,0 +1,22 @@
+%Titulo:Derivada con Formula General (1)
+%Nombre:Vanessa Cruz Huitrón 
+%Grupo:3202
+%Fecha: Jueves 15 de Abril del 2021
+
+%f(x)=x+2
+
+%Limpiar variables
+clear
+%Ejemplo f(x)=a
+%Rango de -3..3 en i=0.1
+x=[-3:1:3];
+%Valor de la funcion
+y=((x-(+2)));
+%Generar grafica 
+plot (x,y);
+%Titulo
+title("f(x)=x+2");
+%Nombre recta x
+xlabel("X");
+%Nombre recta y
+ylabel ("Y");
---
+++ DerivadaConFormulaGeneral2VanessaCruzHuitron3202.m
@@ -0,0 +1,21 @@
+%Titulo:Derivada con Formula General (2)
+%Nombre:Vanessa Cruz Huitrón 
+%Grupo:3202
+%Fecha: Jueves 15 de Abril del 2021
+
+%f(x)=10-x
+
+%Limpiar variables
+clear
+%Ejemplo f(x)=a
+%Rango de -3..3 en i=0.1
+x=[-5:1:5];
+%Valor de la funcion
+y=((10-(x)));
+%Generar grafica 
+plot (x,y);
+%Titulo
+title("f(x)=10-x");
+%Nombre recta x
+xlabel("X");
+
